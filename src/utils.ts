cabb827e0a8a26bc986494655aa6b0c113b4830e
Sam Gaus <sam@gaus.co.uk>
2023-10-24 19:49:08 +0100

    Make disambiguation function much faster

--- src/utils.ts
+++ src/utils.ts
@@ -90,20 +90,33 @@ export function zip(...arrays: unknown[][]) {
   );
 }
 
-export function disambiguate(entities: { id: number; name: string }[]) {
-  return Object.entries(
-    entities
-      .filter(({ name }) => name?.trim())
-      .reduce(
-        (acc, { id, name }) => ({
-          ...acc,
-          [name]: [...(acc[name] ?? []), id],
-        }),
-        {} as Record<string, number[]>,
-      ),
-  ).flatMap(([name, ids]) =>
-    ids.length > 1 ? ids.map((id) => `[${id}]${name}`) : name,
-  );
+type Entity = { id: number; name: string };
+
+const sortEntities = (a: Entity, b: Entity) => {
+  if (a.name > b.name) return -1;
+  if (b.name > a.name) return 1;
+  return 0;
+};
+
+export function disambiguate(entities: Entity[]) {
+  entities.sort(sortEntities);
+
+  const names = [];
+  let disam = false;
+
+  for (let i = 1; i <= entities.length; i++) {
+    const last = entities[i - 1];
+    const current = entities[i];
+
+    if (last.name === current?.name || disam) {
+      names.push(`[${last.id}]${last.name}`);
+      disam = last.name === current?.name;
+    } else {
+      names.push(last.name);
+    }
+  }
+
+  return names;
 }
 
 export function tokenizeAttributes(attributesString: string) {
