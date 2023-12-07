7e6767377cad8e5c46b73e195912934f2029e8d4
Sam Gaus <sam@gaus.co.uk>
2023-12-07 12:33:45 +0000

    Create role for db in the right place

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,7 +1,32 @@
 import { watch } from "data-of-loathing-etl";
 import express from "express";
+import { Pool } from "pg";
 import { postgraphile } from "postgraphile";
 import ConnectionFilterPlugin from "postgraphile-plugin-connection-filter";
+
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+
+await pool.query(`
+  DO
+  $do$
+  BEGIN
+    IF EXISTS (
+      SELECT FROM pg_catalog.pg_roles
+      WHERE rolname = 'readonly_role'
+    ) THEN
+      RAISE NOTICE 'Role "readonly_role" already exists. Nice!';
+    ELSE
+      CREATE ROLE "readonly_role" LOGIN PASSWORD 'readonly_role';
+      GRANT CONNECT ON DATABASE "postgres" TO "readonly_role";
+      GRANT USAGE ON SCHEMA "public" TO "readonly_role";
+      GRANT SELECT ON ALL TABLES IN SCHEMA "public" TO "readonly_role";
+      GRANT SELECT ON ALL SEQUENCES IN SCHEMA "public" TO "readonly_role";
+      ALTER DEFAULT PRIVILEGES IN SCHEMA "public" GRANT SELECT ON TABLES TO "readonly_role";
+      ALTER DEFAULT PRIVILEGES IN SCHEMA "public" GRANT SELECT ON SEQUENCES TO "readonly_role";
+    END IF;
+  END
+  $do$;
+`);
 
 // Start checking for data source updates every 15 minutes
 await watch(15);
