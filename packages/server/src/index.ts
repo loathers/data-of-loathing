ea61e0747f5223a0d10392e124b73ecddf044f55
Sam Gaus <sam@gaus.co.uk>
2024-06-29 16:00:21 +0100

    Give perms to readonly_role each time

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -17,13 +17,14 @@ await pool.query(`
       RAISE NOTICE 'Role "readonly_role" already exists. Nice!';
     ELSE
       CREATE ROLE "readonly_role" LOGIN PASSWORD 'readonly_role';
-      GRANT CONNECT ON DATABASE "postgres" TO "readonly_role";
-      GRANT USAGE ON SCHEMA "public" TO "readonly_role";
-      GRANT SELECT ON ALL TABLES IN SCHEMA "public" TO "readonly_role";
-      GRANT SELECT ON ALL SEQUENCES IN SCHEMA "public" TO "readonly_role";
-      ALTER DEFAULT PRIVILEGES IN SCHEMA "public" GRANT SELECT ON TABLES TO "readonly_role";
-      ALTER DEFAULT PRIVILEGES IN SCHEMA "public" GRANT SELECT ON SEQUENCES TO "readonly_role";
     END IF;
+
+    GRANT CONNECT ON DATABASE "postgres" TO "readonly_role";
+    GRANT USAGE ON SCHEMA "public" TO "readonly_role";
+    GRANT SELECT ON ALL TABLES IN SCHEMA "public" TO "readonly_role";
+    GRANT SELECT ON ALL SEQUENCES IN SCHEMA "public" TO "readonly_role";
+    ALTER DEFAULT PRIVILEGES IN SCHEMA "public" GRANT SELECT ON TABLES TO "readonly_role";
+    ALTER DEFAULT PRIVILEGES IN SCHEMA "public" GRANT SELECT ON SEQUENCES TO "readonly_role";
   END
   $do$;
 `);
