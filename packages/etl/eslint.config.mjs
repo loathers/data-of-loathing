ed1be22615bd72d4827684973e4a9898b462839e
Sam Gaus <sam@gaus.co.uk>
2025-09-17 13:17:15 +0100

    Upgrade deps

--- packages/etl/eslint.config.mjs
+++ packages/etl/eslint.config.mjs
@@ -5,5 +5,12 @@ import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   eslint.configs.recommended,
-  tseslint.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
 );
