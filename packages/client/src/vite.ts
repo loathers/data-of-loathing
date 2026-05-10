2bb392a8be3e9e84363262099e3bf55bb61c367b
Sam Gaus <sam@gaus.co.uk>
2026-05-10 13:41:41 +0100

    Disable process global shim in node polyfills, v3.2.1

--- packages/client/src/vite.ts
+++ packages/client/src/vite.ts
@@ -25,5 +25,8 @@ export default function dol({
     };
   }
 
-  return [nodePolyfills({ include: ["buffer"] }), plugin];
+  return [
+    nodePolyfills({ include: ["buffer"], globals: { process: false } }),
+    plugin,
+  ];
 }
